75c720122e9afc33a9ae38db4c0a068d877a3759
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2017-05-30 17:02:20 +0300

    short stuff

--- scripts/creare_indecsi.sql
+++ scripts/creare_indecsi.sql
@@ -1,21 +1,22 @@
 drop index resourcesCompanyIndex;
 drop index servicesCompanyIndex;
 drop index requestsCompanyIndex;
-drop index ownersCompanyIndex;
+--drop index ownersCompanyIndex;
 drop index ownersUserIdIndex;
 drop index companiesCompanyIndex;
 drop index user_UserIdIndex;
 drop index ownersJoinIndex;
-drop index ownersSecondJoinIndex;
+--drop index ownersSecondJoinIndex;
 drop index orderByCostIndex;
 
-create index  resourcesCompanyIndex on resources(companyid);
-create index servicesCompanyIndex on services(companyid);
-create index requestsCompanyIndex on requests(companyid);
-create index ownersCompanyIndex on owners(companyid);
-create index ownersUserIdIndex on owners(userid);
-create index companiesCompanyIndex on companies(companyid);
-create index user_UserIdIndex on users_(userid);
-create index ownersJoinIndex on owners (userid, companyid);
-create index ownersSecondJoinIndex on owners (companyid, userid);
-create index orderByCostIndex on resources(cost);
+create index resourcesCompanyIndex on resources(companyid); -- checked in getStats (join)
+create index servicesCompanyIndex on services(companyid); -- checked in getStats
+create index requestsCompanyIndex on requests(companyid); -- checked in getStats
+--create index ownersCompanyIndex on owners(companyid); --not implemented yet, for checking if a company has more than one owner
+create index ownersUserIdIndex on owners(userid); -- checked in getStats (select simplu)
+create index companiesCompanyIndex on companies(companyid); -- already indexed by system
+create index user_UserIdIndex on users_(userid); -- already indexed by system
+create index ownersJoinIndex on owners (userid, companyid); -- already indexed by system
+--create index ownersSecondJoinIndex on owners (companyid, userid); -- useless, our mistake
+create index orderByCostIndex on resources(cost); -- custom sort (order by)
+
